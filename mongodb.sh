d59885d3961a255a1044d106bb0398a34bad5cd9
vinodh241 <a.vinodh9072@gmail.com>
2025-05-27 12:02:10 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="\var\log\roboshop-logs"
+#LOGS_FOLDER="\var\log\roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
