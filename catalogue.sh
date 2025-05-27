b5818d588bc811b61fbce3ca0ccb69896eef50c8
vinodh241 <a.vinodh9072@gmail.com>
2025-05-27 13:11:48 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,47 +1,46 @@
 #!/bin/bash
 
-#This is catalogue
-
-USERID=$( id -u )
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
-
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-#LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
-   echo -e "$R ERROR:: Please run this script with root access $N " | tee -a &LOG_FILE
-   exit 1
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-   echo -e "$G youre running with root access $N " | tee -a $LOG_FILE
-   fi
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
-    then 
-       echo -e "Installing $2 is ... $G SUCCESS  $N " | tee -a $LOG_FILE
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-       echo -e "Installing $2 is ... $R FAILURE  $N " | tee -a $LOG_FILE
-       exit 1
-    fi   
-} 
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
-dnf module disable nodejs -y
-VALIDATE $? "disbaling default nodejs"
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling default nodejs"
 
-dnf module enable nodejs:20 -y
-VALIDATE $? "enabling nodejs-20 version"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nodejs:20"
 
-dnf install nodejs -y
-VALIDATE $? "installing nodejs"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing nodejs:20"
 
 id roboshop
 if [ $? -ne 0 ]
@@ -78,7 +77,7 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-STATUS=$(mongosh --host mongodb.vinosh.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host mongodb.vinodh.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
     mongosh --host mongodb.vinodh.site </app/db/master-data.js &>>$LOG_FILE
@@ -86,4 +85,3 @@ then
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
 fi
-
