8b59b6c3d76b2a54f4691887b7e1306caeb26996
vinodh241 <a.vinodh9072@gmail.com>
2025-06-22 13:51:26 +0530

    shell-roboshop-cart-component

---
+++ cart.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)  # here declearing start time 
+USERID=$( id -u )
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+$SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "script started execution: $(date)"
+
+if [ $USERID -ne 0 ]
+then
+   echo -e "$R ERROR:: Please run this script with root access $N " | tee -a &LOG_FILE
+   exit 1
+else
+   echo -e "$G youre running with root access $N " | tee -a $LOG_FILE
+   fi
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then 
+       echo -e "Installing $2 is ... $G SUCCESS  $N " | tee -a $LOG_FILE
+    else
+       echo -e "Installing $2 is ... $R FAILURE  $N " | tee -a $LOG_FILE
+       exit 1
+    fi   
+} 
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? " Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &&>>$LOG_FILE
+VALIDATE $? " Enabling nodejs: 20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? " Installing nodejs " 
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "creating app"
+
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading cart"
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/cart.zip &>>$LOG_FILE
+VALIDATE $? "unzipping cart"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing Dependencies"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Copying cart service"
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable cart &>>$LOG_FILE
+systemctl start cart
+VALIDATE $? "Starting cart"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+
+echo -e " Script execution completed successfully $Y total time taken : $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
