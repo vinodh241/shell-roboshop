bed4fa83eb4fbdabab78fcbeece6495b3c7846c1
vinodh241 <a.vinodh9072@gmail.com>
2025-06-22 12:21:31 +0530

    shell-roboshop-redis-component

--- redis.sh
+++ redis.sh
@@ -36,4 +36,7 @@ VALIDATE $? "Enable redis 7 "
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing redis"
 
+sed -i-e 's/127.0.0.1/0.0.0.0/g' -e '/protect-mode/ c protect-mode no ' /etc/redis/redis.conf
+VALIDATE $? "Editing conf fot to accept remote connections"
+
 
