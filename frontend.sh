1fa438ccc45d8654ece0eab26be119650f104ce8
vinodh241 <a.vinodh9072@gmail.com>
2025-06-22 14:02:12 +0530

    shell-roboshop-frontend-component

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+START_TIME=$( date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -65,3 +65,8 @@ VALIDATE $? "Copying nginx.conf"
 systemctl restart nginx 
 VALIDATE $? "Restarting nginx"
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+
+echo -e " Script execution completed successfully $Y total time taken : $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
