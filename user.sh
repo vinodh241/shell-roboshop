0a2a95a4c2f9eef86200156af8e13723c16cd315
vinodh241 <a.vinodh9072@gmail.com>
2025-06-22 13:05:23 +0530

    shell-roboshop-user-component

--- user.sh
+++ user.sh
@@ -9,6 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+$SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "script started execution: $(date)"
