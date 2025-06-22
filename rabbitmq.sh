dde67549787de1a36d1106b1907df0175c0d8e9b
vinodh241 <a.vinodh9072@gmail.com>
2025-06-22 16:51:05 +0530

    shell-roboshop-rabbitmq-component

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,71 @@
+# #!/bin/bash
+
+# START_TIME=$(date +%s)  # here declearing start time 
+# USERID=$( id -u )
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# LOGS_FOLDER="/var/log/roboshop-logs"
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+# SCRIPT_DIR=$PWD
+
+# mkdir -p $LOGS_FOLDER
+# echo "script started execution: $(date)"
+
+# if [ $USERID -ne 0 ]
+# then
+#    echo -e "$R ERROR:: Please run this script with root access $N " | tee -a &LOG_FILE
+#    exit 1
+# else
+#    echo -e "$G youre running with root access $N " | tee -a $LOG_FILE
+#    fi
+
+
+
+
+# VALIDATE(){
+#     if [ $1 -eq 0 ]
+#     then 
+#        echo -e "Installing $2 is ... $G SUCCESS  $N " | tee -a $LOG_FILE
+#     else
+#        echo -e "Installing $2 is ... $R FAILURE  $N " | tee -a $LOG_FILE
+#        exit 1
+#     fi   
+# } 
+
+
+# cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+# VALIDATE $? "copying rabbitmq repos"
+
+# dnf install rabbitmq-server -y &>>$LOG_FILE
+# VALIDATE $? "installing rabbitmq server" 
+
+# systemctl enable rabbitmq-server
+# VALIDATE $? "enabling rabbitmq"
+
+# systemctl start rabbitmq-server
+# VALIDATE $? "Starting rabbitmq"
+
+# rabbitmqctl add_user roboshop roboshop123
+# VALIDATE $? "Adding user"
+
+# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+# VALIDATE $? "setting up the permissions" 
+
+
+# END_TIME=$(date +%s)
+# TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+
+# echo -e " Script execution completed successfully $Y total time taken : $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
+
 #!/bin/bash
 
-START_TIME=$(date +%s)  # here declearing start time 
-USERID=$( id -u )
+START_TIME=$(date +%s)
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -12,51 +76,47 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
-echo "script started execution: $(date)"
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
-   echo -e "$R ERROR:: Please run this script with root access $N " | tee -a &LOG_FILE
-   exit 1
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-   echo -e "$G youre running with root access $N " | tee -a $LOG_FILE
-   fi
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
 
+echo "Please enter rabbitmq password to setup"
+read -s RABBITMQ_PASSWD
 
-
-
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
-    then 
-       echo -e "Installing $2 is ... $G SUCCESS  $N " | tee -a $LOG_FILE
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-       echo -e "Installing $2 is ... $R FAILURE  $N " | tee -a $LOG_FILE
-       exit 1
-    fi   
-} 
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
-
-cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
-VALIDATE $? "copying rabbitmq repos"
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Adding rabbitmq repo"
 
 dnf install rabbitmq-server -y &>>$LOG_FILE
-VALIDATE $? "installing rabbitmq server" 
+VALIDATE $? "Installing rabbitmq server"
 
-systemctl enable rabbitmq-server
-VALIDATE $? "enabling rabbitmq"
+systemctl enable rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "Enabling rabbitmq server"
 
-systemctl start rabbitmq-server
-VALIDATE $? "Starting rabbitmq"
+systemctl start rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "Starting rabbitmq server"
 
-rabbitmqctl add_user roboshop roboshop123
-VALIDATE $? "Adding user"
-
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-VALIDATE $? "setting up the permissions" 
-
+rabbitmqctl add_user roboshop $RABBITMQ_PASSWD &>>$LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-
-echo -e " Script execution completed successfully $Y total time taken : $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
