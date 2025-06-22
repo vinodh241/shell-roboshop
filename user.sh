c0a0f7743bd711750fa2486876a95987dda2bbd6
vinodh241 <a.vinodh9072@gmail.com>
2025-06-22 11:08:02 +0530

    shell-roboshop-user-component

--- user.sh
+++ user.sh
@@ -52,7 +52,7 @@ else
     echo -e  "$G system user already added $Y .. nothing do $N " 
 fi
 
-mkdir /app 
+mkdir -p /app 
 VALIDATE $? "creating a new directroy"
 
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip 
